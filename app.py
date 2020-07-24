2f22cc839b39338a2dd2c3d2d8a6e05c41907dcc
ParisDescartesLeoYIU <37623992+ParisDescartesLeoYIU@users.noreply.github.com>
2020-07-24 14:11:36 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+"""
+Importation des modules
+"""
+
+from flask import Flask, request, render_template, \
+    redirect, jsonify
+
+from model_sqlite import createTables, \
+    createCode, \
+    getCode, \
+    getAllCode, \
+    updateCode, \
+    createEdition, \
+    getEdition
+
+"""
+Initialisation
+"""
+
+app = Flask(__name__)
+createTables()
+
+languages = [
+    'text',
+    'python',
+    'html',
+    'css',
+    'javascript'
+]
+
+
+
+"""
+Page d'accueil
+"""
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+"""
+Création d'un code
+"""
+
+@app.route('/create')
+def create():
+    uid = createCode() # Créer la ligne
+    print(uid) # Affiche le dernier uid
+    return redirect(f"{request.host_url}edit/{uid}") # Ne fait pas de redirection mais affiche la page
+
+
+"""
+Modification d'un code
+"""
+
+@app.route('/edit/<string:uid>/', methods=['GET'])
+def edit(uid):
+    code = getCode(uid)
+    if code is None:
+        return render_template('error.html', uid=uid)
+    d = dict(uid=uid, code=code, languages=languages,
+             url=f"{request.host_url}view/{uid}")
+    return render_template('edit.html', **d)
+
+
+"""
+Publication d'un code
+"""
+
+@app.route('/publish', methods=['POST'])
+def publish():
+    content = request.form.get('content')
+    uid = request.form.get('uid')
+    language = request.form.get('language')
+
+    # code = Code(uid=uid, content=content, language=language)
+
+    updateCode(uid, content, language)
+    return jsonify({'ok': True})
+
+"""
+Affichage d'un code
+"""
+
+@app.route('/view/<string:uid>/')
+def view(uid):
+    code = getCode(uid)
+
+    if code is None:
+        return render_template('error.html', uid=uid)
+
+    d = dict(uid=uid, code=code, languages=languages,
+             url=f"{request.host_url}view/{uid}")
+
+    return render_template('view.html', **d)
+
+"""
+Administration
+"""
+
+@app.route('/admin/')
+def admin():
+    pass
+
+
+"""
+Vue partielle : Derniers codes modifiées
+"""
+
+@app.route('/_partials/last-added')
+def partialsLastAdded():
+    d = {'last_added': getAllCode()}
+    return render_template('/partials/last-added.html', **d)
+
+
+"""
+Execution de l'application
+"""
+
+if __name__ == '__main__':
+    app.run()
---
+++ model.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+import ast
+import json
+from string import ascii_letters, digits
+from itertools import chain
+from random import choice
+import pickle
+import os
+
+from Code import Code
+
+
+def create_uid(n=9):
+    '''Génère une chaîne de caractères alétoires de longueur n
+   en évitant 0, O, I, l pour être sympa.'''
+    chrs = [c for c in chain(ascii_letters, digits)
+            if c not in '0OIl']
+    return ''.join((choice(chrs) for i in range(n)))
+
+
+def save_doc_as_file(code=Code()):
+    '''Crée/Enregistre le document sous la forme d'un fichier
+    data/uid. Return the file name.
+    '''
+    uid = code.uid
+    if uid is None:
+        code = Code()
+    with open(f'data/{uid}', 'w') as fd:
+        code = str(code)
+        print(code)
+        fd.write(code)
+    return uid
+
+
+def read_doc_as_file(uid):
+    '''Lit le document data/uid'''
+    try:
+        with open(f'data/{uid}') as fd:
+            code = ast.literal_eval(fd.read())
+            code = Code(**code)
+        return code
+    except FileNotFoundError:
+        return None
+
+
+def get_last_entries_from_files(n=10, nlines=10):
+    entries = os.scandir('data')
+    d = []
+    entries = sorted(list(entries),
+                     key=(lambda e: e.stat().st_mtime),
+                     reverse=True)
+    for i, e in enumerate(entries):
+        if i >= n:
+            break
+        if e.name.startswith('.'):
+            continue
+        code = read_doc_as_file(e.name)
+        d.append(code)
+    return d
